aed0ed7d18bcd799b7b565688be0afd7b3bc4af7
Ghufran.Nazir <ghufran.nazir@codefulcrum.com>
2025-07-24 15:48:41 +0500

    Modifed feature

--- newfile.py
+++ newfile.py
@@ -2,8 +2,9 @@ print('this is a newly added python file - Also changed this line as its old')
 print('added this new feature')
 
 def custom_print(*args):
-    for arg in args:
-        print("* ", arg)
+
+    for i, arg in enumerate(args, start=1):
+        print(f"{i}) {arg} ")
 
 
 custom_print("hello", "second")
